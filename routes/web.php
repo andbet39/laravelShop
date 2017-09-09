f6008e0a11924b4dae67a2cde5015ace5507fee7
Adrian Harabula <adrian.harabula@gmail.com>
2017-09-09 22:52:41 +0300

    fix bad redirects to /home route after login, signup or passwordreset

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,6 @@ Route::post('/admin/product/save', 'ProductController@add');
 
 Route::get('/', 'MainController@index');
 
-// Authentication routes...
-// Route::get('auth/login', 'Auth\AuthController@getLogin');
-// Route::post('auth/login', 'Auth\AuthController@postLogin');
-// Route::get('auth/logout', 'Auth\AuthController@getLogout');
-
-
-// Registration routes...
-// Route::get('auth/register', 'Auth\AuthController@getRegister');
-// Route::post('auth/register', 'Auth\AuthController@postRegister');
-
 Route::auth();
 
 Route::get('/addProduct/{productId}', 'CartController@addItem');
@@ -40,6 +30,3 @@ Route::post('/checkout', 'OrderController@checkout');
 Route::get('order/{orderId}', 'OrderController@viewOrder');
 Route::get('order', 'OrderController@index');
 Route::get('download/{orderId}/{filename}', 'OrderController@download');
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
